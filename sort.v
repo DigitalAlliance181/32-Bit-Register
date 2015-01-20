34db30a5f34a93559df36abb44e3211bd86e304a
DigitalAlliance181 <osscubemod@gmail.com>
2015-01-20 13:20:55 -0800

    Create sort.v

---
+++ sort.v
@@ -0,0 +1,21 @@
+// sort.v
+//Lab 2 Part 4.2: Creating a Qsys component to sort
+
+//Chris Bird, Lillie Deas, Kaila Balancio
+
+module scroll (clock, resetn, KEY )
+input clock;
+input resetn;
+
+
+always @ (posedge clock or posedge resetn)
+begin
+	if(resetn)
+
+	else 
+	begin
+
+
+	end
+end
+endmodule
